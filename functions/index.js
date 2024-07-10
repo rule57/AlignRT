6d22b3b5161b024786ac7ab67d660f2432a2f295
rule57 <wjr5232@psu.edu>
2024-07-10 17:56:24 -0400

    oh

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,3 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const {Storage} = require("@google-cloud/storage");
@@ -30,8 +21,15 @@ exports.createGifFromUserImages = functions.https.onRequest(async (req, res) => 
   }
 });
 
+/**
+ * Fetches user images from Cloud Storage.
+ * @param {string} userId - The ID of the user.
+ * @return {Promise<string[]>} - Array of image paths.
+ */
 async function getUserImages(userId) {
-  const [files] = await storage.bucket("your-user-bucket-name").getFiles({prefix: `${userId}/images/`});
+  const [files] = await storage.bucket("your-user-bucket-name").getFiles({
+    prefix: `${userId}/images/`,
+  });
   const imagePaths = [];
 
   for (const file of files) {
@@ -43,6 +41,11 @@ async function getUserImages(userId) {
   return imagePaths;
 }
 
+/**
+ * Uploads GIF to Cloud Storage.
+ * @param {string} gifPath - The local path to the GIF file.
+ * @return {Promise<void>}
+ */
 async function uploadGif(gifPath) {
   await storage.bucket("your-post-bucket-name").upload(gifPath, {
     destination: `gifs/${gifPath.split("/").pop()}`,
